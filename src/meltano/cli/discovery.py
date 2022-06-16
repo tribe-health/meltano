aea6a2655262b59fd5f28c30757cb12c8c091c0c
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2022-06-16 17:58:08 -0500

    Use rich for meltano discover

--- src/meltano/cli/discovery.py
+++ src/meltano/cli/discovery.py
@@ -1,14 +1,50 @@
 """Discoverable Plugins CLI."""
 
+from __future__ import annotations
+
 import click
+from rich.console import Console
+from rich.table import Table
 
 from meltano.core.hub import MeltanoHubService
+from meltano.core.hub.schema import IndexedPlugin
 from meltano.core.legacy_tracking import LegacyTracker
 from meltano.core.plugin import PluginType
 from meltano.core.project import Project
 
 from . import cli
 from .params import pass_project
+
+
+def _construct_table() -> Table:
+    table = Table(title="MeltanoHub Plugins")
+    table.add_column("Name", style="bold")
+    table.add_column("Type", style="magenta")
+    table.add_column("Variant Name", style="italic")
+    table.add_column("Notes")
+    table.add_column("MeltanoHub Link", style="cyan")
+
+    return table
+
+
+def _add_rows_rows(
+    table: Table,
+    plugin_type: PluginType,
+    plugins: dict[str, IndexedPlugin],
+):
+    for plugin_name, plugin in plugins.items():
+        for variant_name in plugin.variants:
+            table.add_row(
+                plugin_name,
+                plugin_type.descriptor,
+                variant_name,
+                (
+                    "[green]Default variant[/]"
+                    if variant_name == plugin.default_variant
+                    else ""
+                ),
+                f"https://hub.meltano.com/{plugin_type}/{plugin_name}--{variant_name}",
+            )
 
 
 @cli.command(short_help="List the available plugins in Meltano Hub and their variants.")
@@ -23,6 +59,8 @@ def discover(project: Project, plugin_type: str):
     \b\nRead more at https://docs.meltano.com/reference/command-line-interface#discover
     """
     hub_service = MeltanoHubService(project)
+    console = Console()
+
     if plugin_type == "all":
         plugin_types = [
             plugin_type for plugin_type in list(PluginType) if plugin_type.discoverable
@@ -30,11 +68,11 @@ def discover(project: Project, plugin_type: str):
     else:
         plugin_types = [PluginType.from_cli_argument(plugin_type)]
 
+    table = _construct_table()
+
     for idx, discovered_plugin_type in enumerate(plugin_types):
         if idx > 0:
             click.echo()
-
-        click.secho(f"{str(discovered_plugin_type).capitalize()}", fg="green")
 
         try:
             plugin_type_index = hub_service.get_plugins_of_type(discovered_plugin_type)
@@ -46,13 +84,10 @@ def discover(project: Project, plugin_type: str):
             )
             continue
 
-        for plugin_name, plugin in plugin_type_index.items():
-            click.echo(plugin_name, nl=False)
+        _add_rows_rows(table, discovered_plugin_type, plugin_type_index)
 
-            if len(plugin.variants) > 1:
-                click.echo(f", variants: {', '.join(plugin.variant_labels)}")
-            else:
-                click.echo()
+    with console.pager(styles=True):
+        console.print(table)
 
     tracker = LegacyTracker(project)
     tracker.track_meltano_discover(plugin_type=plugin_type)
